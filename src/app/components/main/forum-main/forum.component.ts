8e44a19a11b30ef3e7c3d261cdd311a892543338
lamstroning <lexashob@yandex.ru>
2020-11-29 08:21:30 +0300

    fix forum

--- src/app/components/main/forum-main/forum.component.ts
+++ src/app/components/main/forum-main/forum.component.ts
@@ -9,57 +9,115 @@ export class ForumComponent implements OnInit {
   topicList = [
     {
       id: 0,
+      img: '/assets/img/icons/line-1.png',
       name: 'Управление технологическим процессом. Цифровая сеть ...',
       favorite: false,
-      date: '12.11'
+      date: '12.11',
+      like: 21,
+      comment: 12,
+      hashTags: [
+        'Управление',
+        'Технологии',
+        'Процесс'
+      ]
     },
     {
       id: 1,
-      name: 'Управление технологическим процессом. Цифровая сеть ...',
+      img: '/assets/img/icons/line-2.png',
+      name: 'Цифровое управление компанией',
       favorite: false,
-      date: '12.11'
+      date: '15.11',
+      like: 125,
+      comment: 25,
+      hashTags: [
+        'Сети',
+        'Экономика',
+        'ЛЭП'
+      ]
     },
     {
       id: 2,
-      name: 'Управление технологическим процессом. Цифровая сеть ...',
+      img: '/assets/img/icons/line-3.png',
+      name: 'Дополнительные сервисы',
       favorite: false,
-      date: '12.11'
+      date: '25.11',
+      like: 25,
+      comment: 125,
+      hashTags: [
+        'Фильтр',
+        'Технологии',
+        'Воздух'
+      ]
     },
     {
       id: 3,
-      name: 'Управление технологическим процессом. Цифровая сеть ...',
+      img: '/assets/img/icons/line-4.png',
+      name: 'Комплексная система информационной безопасности',
       favorite: false,
-      date: '12.11'
+      date: '12.10',
+      like: 31,
+      comment: 154,
+      hashTags: [
+        'Кризис',
+        'Технологии',
+        'Сети'
+      ]
     },
     {
-      id: 0,
+      id: 4,
+      img: '/assets/img/icons/line-1.png',
       name: 'Управление технологическим процессом. Цифровая сеть ...',
       favorite: false,
-      date: '12.11'
+      date: '12.11',
+      like: 1,
+      comment: 15,
+      hashTags: [
+        'Кризис',
+        'Экономика',
+        'Процесс'
+      ]
     },
     {
-      id: 0,
-      name: 'Управление технологическим процессом. Цифровая сеть ...',
+      id: 5,
+      img: '/assets/img/icons/line-2.png',
+      name: 'Цифровое управление компанией',
       favorite: false,
-      date: '12.11'
+      date: '15.11',
+      like: 25,
+      comment: 125,
+      hashTags: [
+        'Кризис',
+        'Трансформатор',
+        'Процесс'
+      ]
     },
     {
-      id: 0,
-      name: 'Управление технологическим процессом. Цифровая сеть ...',
+      id: 6,
+      img: '/assets/img/icons/line-3.png',
+      name: 'Дополнительные сервисы',
       favorite: false,
-      date: '12.11'
+      date: '25.11',
+      like: 5,
+      comment: 1,
+      hashTags: [
+        'Кризис',
+        'Трансформатор',
+        'Процесс'
+      ]
     },
     {
-      id: 0,
-      name: 'Управление технологическим процессом. Цифровая сеть ...',
+      id: 7,
+      img: '/assets/img/icons/line-3.png',
+      name: 'Комплексная система информационной безопасности',
       favorite: false,
-      date: '12.11'
-    },
-    {
-      id: 0,
-      name: 'Управление технологическим процессом. Цифровая сеть ...',
-      favorite: false,
-      date: '12.11'
+      date: '12.10',
+      like: 4,
+      comment: 15,
+      hashTags: [
+        'Кризис',
+        'Трансформатор',
+        'Управление'
+      ]
     }
   ];
   constructor() { }
